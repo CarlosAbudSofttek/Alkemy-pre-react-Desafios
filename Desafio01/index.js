f1cfcb402fb38312451a79c4f8337c33a8647c1a
Carlos Enrique Abud <carlos.abud@softtek.com>
2021-08-26 11:09:06 -0300

    ajustes logica desafío 1

--- Desafio01/index.js
+++ Desafio01/index.js
@@ -1,9 +1,8 @@
-var calculate = () =>{
-    let from = document.getElementById("from").value;
-    let to = document.getElementById("to").value;
-    let result;
-    for(var i = Number(from); i<=Number(to); i+=2){
-        result = result ? `${result}, ${i}` : i;
-    }
-    document.getElementById("results").innerText = result;
+var calculate = () => {
+    let from = Number(document.getElementById("from").value);
+    from += from % 2;//Ajusta al par inmediato superior
+    let to = Number(document.getElementById("to").value);
+    //Las posiciones del Array son la mitad + 1, ya que los bordes son inclusivos
+    let result = Array.from({ length: (to - from) / 2 + 1 }, (_, i) => from + (i * 2));
+    document.getElementById("results").innerText = result.join(', ');
 }
